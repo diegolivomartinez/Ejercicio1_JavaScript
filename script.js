e9fb815c2901828eb0cd24514051070622520dfd
Diego Olivo <diego.olivomartinez.23@campusfp.es>
2024-03-01 10:36:01 +0100

    ej1

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+console.log("Mi primer ejercicio de JS");
+
+// script.js
+
+// Variables
+let nombre = "Diego"; // Reemplaza "TuNombre" con tu nombre real
+let apellidos = "Olivo Martínez"; // Reemplaza "TusApellidos" con tus apellidos reales
+let animal = "Oso Polar"; // Reemplaza "TuAnimalPreferido" con tu animal preferido
+
+// Mostrar en la consola
+console.log("Nombre:", nombre);
+console.log("Apellidos:", apellidos);
+console.log("Animal preferido:", animal);
